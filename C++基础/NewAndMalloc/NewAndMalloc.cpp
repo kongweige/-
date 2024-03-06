9ed10f8976db08de729702f5a057ce59bf5c1c7e
kongweige <2717356813@qq.com>
2024-03-06 14:38:21 +0800

    feat: wild pointer

---
+++ C++基础/NewAndMalloc/NewAndMalloc.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+/* malloc和new的区别: */
+// 1.malloc 需要申请指定字节的内存，new不需要会自动自己算所需要的字节数
+// 2.new的底层是由mallc分配内存之后，调用构造函数创建对象
+// 3.new返回类型指针，类型安全，malloc返回void*，需要强转成所需的类型
+
+int main()
+{
+  // 申请内存并确定内存是否申请成功
+  char *str = new char[100];
+  assert(str != nullptr);
+
+  delete[] str; // 释放动态分配的数组内存
+  str = nullptr;
+  
+  std::vector<int>* vec = new std::vector<int>(10); // 动态分配一个 vector 对象
+
+  std::cout << sizeof(*vec) << std::endl; // 输出 vector 对象的大小
+  
+  delete vec;    // 释放动态分配的 vector 对象
+  vec = nullptr; // 将指针设置为nullptr
+
+  return 0;
+}
--- C++基础/PointerAndReference/PointerAndReference.cpp
+++ C++基础/PointerAndReference/PointerAndReference.cpp
@@ -1,8 +1,16 @@
 /* 指针和引用的区别: */
-// 1.指针本质是一个地址，有自己的内存空间，引用只是一个别名
-// 2.指针可以指向其他对象，引用不能，在初始化之后就不能改变了
-// 3.指针可以初始化为nullptr，而引用必须初始化为一个已有对象的引用
+// 1. 指针本质是一个地址，有自己的内存空间，引用只是一个别名
+// 2. 指针可以指向其他对象，引用不能，在初始化之后就不能改变了
+// 3. 指针可以初始化为nullptr，而引用必须初始化为一个已有对象的引用
 
+/* 野指针: */
+// 野指针是指未被初始化或指向无效内存地址的指针
+// 1. 指针未被初始化，但被使用
+// 2. 指针被释放后未置空，继续被使用
+// 3. 指针被错误地赋予了一个无效的地址
+
+/* 悬挂指针: */
+// 指向已经释放的指针，导致未定义行为
 
 #include <iostream>
 
@@ -10,6 +18,20 @@ void printVal(double& val)
 {
   std::cout << sizeof(val) << std::endl;
 }
+
+class MyClass 
+{
+public:
+  MyClass() 
+  {
+    std::cout << "MyClass Constructor" << std::endl;
+  }
+  
+  ~MyClass() 
+  {
+    std::cout << "MyClass Destructor" << std::endl;
+  }
+};
 
 int main()
 {
@@ -19,5 +41,30 @@ int main()
   pter = &val;
   std::cout << *pter << std::endl;
   printVal(val);
+
+  // 野指针
+  // int* ptr;
+  // std::cout << ptr << std::endl; // 未初始化
+
+  // int* ptr = new int(5);
+  // free(ptr);
+  // std::cout << ptr << std::endl; // 位置空 继续使用
+
+  // int *invalidPtr = nullptr;
+  // std::cout << *invalidPtr << std::endl; // 错误地解引用指针
+
+  // 悬挂指针  
+  MyClass* ptr = new MyClass(); // 分配内存并创建对象
+  delete ptr; // 释放对象
+  ptr = nullptr; // 将指针置空
+
+  // 悬挂指针：指向已经释放的内存
+  std::cout << ptr << std::endl; // 输出悬挂指针的值
+  
+  // 尝试使用悬挂指针
+  // 悬挂指针的行为是未定义的，可能导致程序崩溃或其他意外结果
+  // 由于是未定义的行为，实际结果可能因系统环境、编译器等而异
+  // std::cout << *ptr << std::endl;
+
   return 0;
 }
