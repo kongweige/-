ae8de7229c989c001f75411b2396c33837cf1d39
kongweige <2717356813@qq.com>
2024-03-07 14:17:37 +0800

    feat: virtual inline

--- C++基础/inline/inline.cpp
+++ C++基础/inline/inline.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+// 虚函数（virtual）可以是内联函数（inline）吗：
+// 虚函数可以是内联函数，内联是可以修饰虚函数的，但是当虚函数表现多态性的时候不能内联
+// 内联是在编译期建议编译器内联，而虚函数的多态性在运行期，编译器无法知道运行期调用哪个代码，因此虚函数表现为多态性时（运行期）不可以内联。
+
 class MyClass
 {
 public:
@@ -13,18 +17,22 @@ public:
 
   inline virtual void who()
   {
-    std::cout << "I am Base\n" << std::endl;
+    std::cout << "I am Base" << std::endl;
   }
 
 private:
   int value = 10;
 };
 
-class SunClass
+class SunClass : public MyClass
 {
 public:
   SunClass(){}
   
+  inline virtual void who()
+  {
+    std::cout << "I am Sun" << std::endl;
+  }
 };
 
 // 类外需要显示建议内联
@@ -35,7 +43,14 @@ inline int MyClass::func()
 
 int main()
 {
-  MyClass my_calss;
+	// 此处的虚函数 who()，是通过类（MyClass）的具体对象（b）来调用的，编译期间就能确定了，所以它可以是内联的，但最终是否内联取决于编译器
+	MyClass b;
+	b.who();
+
+	// 此处的虚函数是通过指针调用的，呈现多态性，需要在运行时期间才能确定，所以不能为内联
+  MyClass* my_calss = new SunClass();
+  my_calss->who();
+
 //优点:
 // 内联函数 通过在函数调用处展开代码  避免了函数调用时涉及到的参数传递 减少了函数调用开销
 // 内联函数在代码展开时，会做安全类型检查和自动类型转换
@@ -44,8 +59,11 @@ int main()
 // 内联函数通过代码膨胀提高效率，如果函数内部执行时间过长，效率会下降，而且在调用处复制代码，也会带来内存空间的消耗
 // 内敛函数在函数库升级的时候需要重新编译
 // 内联函数只是建议内敛，最终取决于编译器
-  std::cout << my_calss.rturnValue() << std::endl;
-  std::cout << my_calss.func() << std::endl;
+  std::cout << my_calss->rturnValue() << std::endl;
+  std::cout << my_calss->func() << std::endl;
  
+  delete my_calss;
+  my_calss = nullptr;
+
   return 0;
 }
---
+++ 数据结构与算法/STL/vector/vector.cpp
@@ -0,0 +1,72 @@
+/* vector 底层实现原理: */
+// vector的底层由数组实现，通过3个迭代器指针来动态管理容器，当插入元素容器的容量不够时需要对容器进行扩容
+#include <iostream>
+#include <vector>
+
+void growPushBack(std::vector<int> &vec, uint16_t size)
+{
+  for(int i = 0; i < 100; i++)
+  { 
+    vec.push_back(i);
+    if(size != vec.capacity())
+    {
+      std::cout << "Capacity changed: " << vec.capacity() << std::endl;
+    }
+  }
+}
+
+int main()
+{
+  std::vector<int> my_vector;
+
+  for(int i = 0; i < 10; i++) {
+    my_vector.push_back(i);
+  }
+  
+  for(auto it = my_vector.begin(); it != my_vector.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << std::endl;
+
+  // size() 返回数组中元素的个数
+  std::cout << "size of my_vector:" << my_vector.size() << std::endl;
+
+  // max_size() 返回系统限制的最大数量 
+  std::cout << "max size of my_vector:" << my_vector.max_size() << std::endl;
+
+  // resize() 调整容器大小使其包含n个元素 （控制的是容器的大小）
+  my_vector.resize(5);
+  my_vector.resize(8,100);
+  my_vector.resize(12);
+  for(auto it = my_vector.begin(); it != my_vector.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << std::endl;
+  
+  // capacity() 返回当前vector所分配的空间大小
+  std::cout << "size: " << (int) my_vector.size() << std::endl;
+  std::cout << "capacity: " << (int) my_vector.capacity() << std::endl;
+  
+
+  // reserve()请求vector容量至少足以包含n个元素（控制的是容器的容量）vs(1.5倍扩容)gcc(2倍扩容)
+  // 直接扩充到已经确定的大小，可以减少多次开辟、释放空间的问题（优化 push_back），就可以提高效率，其次还可以减少多次要拷贝数据的问题
+  uint16_t vec_size = 0;
+
+  std::vector<int> vec_int_a;
+  vec_size = vec_int_a.capacity();
+ 
+  std::cout << "Making vec_int_a growing:" << std::endl;
+  std::cout << "current size: " << vec_size << std::endl;
+  growPushBack(vec_int_a, vec_size);  //声明vector后未使用reserve，直接进行push_back操作 
+  
+  //-------------------------------------- //
+  
+  std::vector<int> vec_int_b;
+  vec_int_b.reserve(100);
+  vec_size = vec_int_b.capacity();
+  std::cout << "Making vec_int_a growing:" << std::endl;
+  std::cout << "current size: " << vec_size << std::endl;
+  growPushBack(vec_int_b, vec_size);
+
+
+
+  return 0;
+}
